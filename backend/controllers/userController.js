31453fe8620b4fa40890895e4a923f444d948b84
pathum lakshan <123624162+pathumlak@users.noreply.github.com>
2026-02-01 00:53:43 +0530

    refactor createUser function to hash password before saving user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,7 @@
 import User from "../models/userModel.js";
 // import asynchandler
 import asyncHandler from "../middlewares/asyncHandler.js";
+import bcrypt from "bcryptjs";
 
 const createUser = asyncHandler(async (req, res) => {
   //   res.send("hello");
@@ -14,21 +15,21 @@ const createUser = asyncHandler(async (req, res) => {
   if (!username || !email || !password) {
     throw new Error("Please add all fields");
   }
-
   //   now we check the user is exist or not using the email
   // use the findone method for checking the email
   const userExists = await User.findOne({ email });
   if (userExists) res.status(400).send("User already exists");
-
-//   create new user
-const newUser = new User({username, email, password});
-try {
-    await newUser.save()
-    res.status(201).send("User created successfully")
-} catch (error) {
-    res.status("Invalid User data")
-    
-}
+  // bcrypt the user password using salt
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  //   create new user
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    res.status(201).send("User created successfully");
+  } catch (error) {
+    res.status("Invalid User data");
+  }
 });
 
 export default createUser;
