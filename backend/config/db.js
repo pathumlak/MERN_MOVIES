16796db24607a32201a696ca6a11975a9151d378
pathum lakshan <123624162+pathumlak@users.noreply.github.com>
2026-01-31 23:36:52 +0530

    initialize the db config and environment

---
+++ backend/config/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+
+const connectDB =  async () =>{
+    try {
+        await mongoose.connect(process.env.MONGO_URI )
+        console.log(`Successfully connected to the DB 👌😘`)
+        
+    } catch (error) {
+        console.error(`Error: ${error.message}`)
+        process.exit(1)
+    }
+}
+
+
+export default connectDB;
