35f05899b184dc052d79e09cd62d8acfd2bb1256
pathum lakshan <123624162+pathumlak@users.noreply.github.com>
2026-02-01 00:47:48 +0530

    refactor createUser function to include user validation and error handling

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,8 +4,31 @@ import User from "../models/userModel.js";
 import asyncHandler from "../middlewares/asyncHandler.js";
 
 const createUser = asyncHandler(async (req, res) => {
-  res.send("hello");
-});
+  //   res.send("hello");
+  // create a user credendials
+  const { username, email, password } = req.body;
+  //   just view that request body is working or not
+  //   console.log(username, email, password);
 
+  //   NOW if user don't add any of fields then got the error
+  if (!username || !email || !password) {
+    throw new Error("Please add all fields");
+  }
+
+  //   now we check the user is exist or not using the email
+  // use the findone method for checking the email
+  const userExists = await User.findOne({ email });
+  if (userExists) res.status(400).send("User already exists");
+
+//   create new user
+const newUser = new User({username, email, password});
+try {
+    await newUser.save()
+    res.status(201).send("User created successfully")
+} catch (error) {
+    res.status("Invalid User data")
+    
+}
+});
 
 export default createUser;
