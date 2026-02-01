b5926ada8600caf2efec77c7e590a3a223deded7
pathum lakshan <123624162+pathumlak@users.noreply.github.com>
2026-02-01 13:08:09 +0530

    implement user authentication with Redux; add login functionality and navigation updates

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import './index.css';
-import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router';
-import { RouterProvider } from 'react-router-dom';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router";
+import { RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
 
+import Login from "./pages/auth/Login.jsx";
 
 const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<App />} />
-  )
-)
+  createRoutesFromElements(<Route path="/" element={<App />} >
+    <Route path="/login" element={<Login />} />
+  </Route>),
+);
 
-createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
-)
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>,
+);
--- frontend/src/pages/auth/Navigation.jsx
+++ frontend/src/pages/auth/Navigation.jsx
@@ -1,10 +1,20 @@
 import { useState } from "react";
 import "./Navigation.css";
-import { Link } from "react-router";
-import {FaHeart} from "react-icons/fa"
-import { AiOutlineHome, AiOutlineLogin, AiOutlineShopping, AiOutlineShoppingCart, AiOutlineUserAdd } from "react-icons/ai";
+import { Link, useNavigate } from "react-router";
+import { FaHeart } from "react-icons/fa";
+import {
+  AiOutlineHome,
+  AiOutlineLogin,
+  AiOutlineShopping,
+  AiOutlineShoppingCart,
+  AiOutlineUserAdd,
+} from "react-icons/ai";
+import { useSelector, useDispatch } from "react-redux";
+import { useLoginMutation } from "../../redux/api/usersApiSlice.js";
+import { logout } from "../../redux/features/auth/authSlice.js";
 
 function Navigation() {
+  const { userInfo } = useSelector((state) => state.auth);
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const [showSidebar, setShowSidebar] = useState(false);
 
@@ -19,6 +29,20 @@ function Navigation() {
     setShowSidebar(false);
   };
 
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [logoutApiCall] = useLoginMutation();
+
+  const logoutHandler = async () => {
+    try {
+      await logoutApiCall().unwrap();
+      dispatch(logout());
+      navigate("/login");
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <div
       style={{ zIndex: 999 }}
@@ -55,28 +79,42 @@ function Navigation() {
           <span className="hidden nav-item-name mt-[3rem]">FAVORITE</span>{" "}
         </Link>
       </div>
-{/* login screen */}
-<ul>
-    <li>
-        <Link
-          to="/login"
-          className="flex items-center transition-transform transform hover:translate-x-2"
-        >
-          <AiOutlineLogin size={26} className="mr-2 mt-[3rem]" />
-          <span className="hidden nav-item-name mt-[3rem]">LOGIN</span>{" "}
-        </Link>
-    </li>
-     <li>
-        <Link
-          to="/register"
-          className="flex items-center transition-transform transform hover:translate-x-2"
-        >
-          <AiOutlineUserAdd size={26} className="mr-2 mt-[3rem]" />
-          <span className="hidden nav-item-name mt-[3rem]">REGISTER</span>{" "}
-        </Link>
-    </li>
-</ul>
 
+      <div className="relative">
+        <button
+          onClick={toggleDropdown}
+          className="flex items-center text-gray-8000 focu:outline-none"
+        >
+          {userInfo ? (
+            <span className="text-white">{userInfo.name}</span>
+          ) : (
+            <></>
+          )}
+        </button>
+      </div>
+      {/* login screen */}
+      <ul>
+        <li>
+          <Link
+            to="/login"
+            className="flex items-center transition-transform transform hover:translate-x-2"
+          >
+            <AiOutlineLogin size={26} className="mr-2 mt-[3rem]" />
+            <span className="hidden nav-item-name mt-[3rem]">LOGIN</span>{" "}
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/register"
+            className="flex items-center transition-transform transform hover:translate-x-2"
+          >
+            <AiOutlineUserAdd size={26} className="mr-2 mt-[3rem]" />
+            <span className="hidden nav-item-name mt-[3rem]">
+              REGISTER
+            </span>{" "}
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 }
